8dad3a863065fab922eafa3074e11ea5104375b0
tonney_magaki <magakitonny@gmail.com>
2017-11-09 04:54:48 +0300

    changes

---
+++ js/rotate.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  var rotate = function(){
+    $(".demo1 .rotate").textrotator({
+        animation: "spin",
+        speed: 2000
+      });
+  }
+
+  rotate();
+});
